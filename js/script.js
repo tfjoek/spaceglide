af544fff47dafa49fa5ecbdd691f6038ffe5dcc3
tfjoek <125345194+tfjoek@users.noreply.github.com>
2023-03-22 09:07:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-    // const on variable jota ei voi vaihta basically
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
 about.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
   if (id) {
-    // tää poistaa sen active staten muista napeista ku painat yhtä
+    // poistaa valittu muista napeista
     btns.forEach(function (btn) {
       btn.classList.remove("active");
     });
     e.target.classList.add("active");
-    // piilottaa muut info hommat ku painaa nappi on active
+    // piilottaa muut artikkelit
     articles.forEach(function (article) {
       article.classList.remove("active");
     });
